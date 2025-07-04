2c6df5ac94b1dd9734bf0a0c650b1c4a6d692439
MariaLu07 <malupdeo07@gmail.com>
2025-07-04 11:31:59 -0300

    Create alimentos.py

---
+++ alimentos.py
@@ -0,0 +1,12 @@
+alimento = input("Digite seu alimento: ")
+carboidrato = input("Quanto carboidrato tem no seu prato? ")
+carboidrato = input("Quanto carboidrato tem no seu prato? ")
+carboidrato = input("Quanto carboidrato tem no seu prato? ")
+carboidrato = input("Quanto carboidrato tem no seu prato? ")
+
+
+print(f"\nSeu nome completo é {nome}")
+print(f"Sua data de nascimento é {nascimento}")
+print(f"Seu gênero é {genero}")
+print(f"Você tem {altura} centímetros de altura")
+print(f"Você tem {peso}Kg inicialmente")
