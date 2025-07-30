be6ecd374e6bf014d0d40ab26812c3da19601d8b
MariaLu07 <malupdeo07@gmail.com>
2025-07-30 08:42:32 -0300

    Update sono.py

--- back/sono.py
+++ back/sono.py
@@ -9,34 +9,38 @@ while True:
     escolha = input("\nDigite 1, 2 ou 3: ")
 
     if escolha == '1':
-        data = input("Digite a data do consumo: ")
-        hora_inicio = input("Digite a hora do consumo: ")
-        hora_final = input("Digite a quantidade de água consumida (em ml): ")
-        qualidade = input("Digite a quantidade de água consumida (em ml): ")
-        hora_final = input("Digite a quantidade de água consumida (em ml): ")
+        data = input("Digite a data do registro: ")
+        hora_inicio = input("Digite a hora inicial do sono: ")
+        hora_final = input("Digite a hora final do sono: ")
+        qualidade = input("Digite a qualidade do seu sono (de 1 a 10): ")
+        observacoes = input("Digite alguma observação do seu sono: ")
 
         registro = {
             'data': data,
-            'hora': hora,
-            'quantidade': quantidade
+            'hora_inicio': hora_inicio,
+            'hora_final': hora_final,
+            'qualidade': qualidade,
+            'observacoes': observacoes
         }
 
         registro_sono.append(registro)
-        print("Consumo de água registrado com sucesso!")
+        print("Sono registrado com sucesso!")
 
     elif escolha == '2':
         if registro_sono:
-            print("\nSeus registros de consumo de água:")
+            print("\nSeus registros de sono:")
             for i, registro in enumerate(registro_sono, 1):
                 print(f"\nRegistro {i}:")
                 print(f" Data: {registro['data']}")
-                print(f" Hora: {registro['hora']}")
-                print(f" Quantidade: {registro['quantidade']}")
+                print(f" Hora inicial: {registro['hora']}")
+                print(f" Hora final: {registro['quantidade']}")
+                print(f" Qualidade: {registro['quantidade']}")
+                print(f" Hora final: {registro['quantidade']}")
         else:
-            print("\nVocê ainda não registrou nenhum consumo de água.")
+            print("\nVocê ainda não registrou nenhum sono.")
 
     elif escolha == '3':
-        print("\nSaindo do programa. Registro de consumo de água encerrado!")
+        print("\nSaindo do programa. Registro de sono encerrado!")
         break
 
     else:
