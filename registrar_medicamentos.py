759578b9ebae078204a1a3b3158895492bf20ba8
MariaLu07 <malupdeo07@gmail.com>
2025-07-30 08:26:51 -0300

    Delete registrar_medicamentos.py

--- registrar_medicamentos.py
+++
@@ -1,7 +0,0 @@
-nome = input("Digite o nome do medicamento: ")
-data = input("Digite a hora do registro: ")
-hora = input("Digite a hora do registro: ")
-
-print(f"\nMedicamento: {nome}")
-print(f"Data: {data}")
-print(f"Hora: {hora}")
