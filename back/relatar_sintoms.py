6759c6de1b13d2d7b1c136cf3409d1850b75b936
MariaLu07 <malupdeo07@gmail.com>
2025-08-08 01:03:53 -0300

    Create relatar_sintoms.py

---
+++ back/relatar_sintoms.py
@@ -0,0 +1,7 @@
+data_hora = input("Digite a data e hora: ")
+intensidade = input("Digite a intensidade: ")
+observacoes = input("Digite as observações: ")
+
+print(f"\nA data e hora registradas são: {data_hora}")
+print(f"A intensidade informada foi: {intensidade}")
+print(f"As observações feitas foram: {observacoes}")
